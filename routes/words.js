0ab041f5ee986417eb83347920465a5317641418
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-08 04:35:12 -0400

    completed word of the day idk unable to create submit repositry so doing this way

--- routes/words.js
+++ routes/words.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { readFile } = require('fs').promises;
 
 router.get('/', (req, res)=>{
-req.send('word home page');
-
+    res.send('word home page');
 });
 
-router.get('/wotd', (req, res)=>{
-res.send('word of the day page');
-
-
+router.get('/wotd', async (req, res)=>{
+    const data = await getwordfromdictionary();
+    res.render('wotd', data);
 });
 
 module.exports = router;
+
+async function getwordfromdictionary(){
+    try{
+        const file = await readFile('./resources/allwords.txt','utf8');
+
+        const words = file.split('\n');
+
+        const randomLine =
+            words[Math.floor(Math.random() * words.length)];
+
+        const [word, part, definition] = randomLine.split('|');
+
+        return { word, part, definition };
+
+    }catch(err){
+        console.log(err);
+    }
+}
