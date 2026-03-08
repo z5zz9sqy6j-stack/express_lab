0d2aa0fe86ef3e643e754426b3c63f4ab46f2ad4
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-03-07 22:50:21 -0500

    setup ejs and first template

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
-app.get('/',(req, res)=>{
+
+app.set('view engine', 'ejs');
+app.get('/',
+    (req, res)=>{
 console.log('Here');
-res.render('index');
+res.render('index', {username: "Jenil" });
 });
-app.get('/status',(req, res)=>{
-res.download('server.js');
+app.get('/users', (req, res)=>{
+res.send('User List');
+});
+app.get('/users/new', (req, res)=>{
+res.send('User New Form');
 });
 app.listen(3030);
